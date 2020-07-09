0a29b3934af5dc4d7e74fcaf4c87eeee9c84c6e6
Yurii Kozyrev <1000and39@gmail.com>
2020-07-09 17:07:21 +0300

    Домашнее задание к уроку 7

---
+++ Lesson_6/server/Server.java
@@ -0,0 +1,80 @@
+package Lesson_6.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Server {
+    private Vector<ClientHandler> clients;
+
+    public Server() {
+        ServerSocket server = null;
+        Socket socket = null;
+
+        try {
+            AuthService.connect();
+            server = new ServerSocket(8189);
+            System.out.println("Сервер запущен!");
+            clients = new Vector<>();
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("Клиент подключился");
+                new ClientHandler(this, socket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            AuthService.disconnect();
+        }
+    }
+
+    public void subscribe(ClientHandler client) {
+        clients.add(client);
+    }
+
+    public void unsubscribe(ClientHandler client) {
+        clients.remove(client);
+    }
+
+    public void broadcastMsg(String msg) {
+        for (ClientHandler o: clients) {
+            o.sendMsg(msg);
+        }
+    }
+
+    public void personalMsg(ClientHandler from, String nickTo, String msg){
+        for (ClientHandler o: clients) {
+            if (o.getNick().equals(nickTo)){
+                o.sendMsg("from " + from.getNick() + " :  " + msg);
+                from.sendMsg("to " + nickTo + ": " + msg);
+                return;
+            }
+        }
+        from.sendMsg("Клиент с именем: " + nickTo + " не найден");
+    }
+
+    public boolean isNickBusy (String nick){
+        for (ClientHandler o: clients){
+            if(o.getNick().equals(nick)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
